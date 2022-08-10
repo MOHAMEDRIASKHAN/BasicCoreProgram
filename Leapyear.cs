33dde16ce87dc9269d6f7fe4e72b5916e71dd191
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 12:21:49 +0530

    Leapyear.cs

---
+++ Leapyear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class Leapyear
+    {
+        public static void CalculateLeapYear()
+        {
+            Console.WriteLine("Enter the Year : ");
+            int year = int.Parse(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                Console.WriteLine(" {0} is a leap year ", year);
+            }
+            else
+            { 
+                Console.WriteLine(" {0} is not a learyear ", year);
+                    }
+        }
+    }
+}
