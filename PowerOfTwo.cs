626bcacd33627f77c5517a29c4290894ded2e13b
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 12:25:33 +0530

    PowerOfTwo.cs

---
+++ PowerOfTwo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class PowerOfTwo
+    {
+
+        public static bool CalculateThePower(int n)
+        {
+            if (n == 0)
+                return false;
+
+            return (int)(Math.Ceiling((Math.Log(n) / Math.Log(2)))) == (int)(Math.Floor(((Math.Log(n) / Math.Log(2)))));
+
+            if (CalculateThePower(31))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            }
+
+        }
+    }
+
