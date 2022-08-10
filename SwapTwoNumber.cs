b87fbe577f83a869b8c974e6dae1a71400f648e3
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 12:32:59 +0530

    SwapTwoNumber.cs

---
+++ SwapTwoNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class SwapTwoNumber
+    {
+        public static void DisplaySwapTwoNumber()
+        {
+            int a = 5;
+            int b = 10;
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("after swap the value is " + a +" " + b);
+        }
+    }
+}
