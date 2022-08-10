5aab649bacea86f67d3bac237cd7fc068387c936
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 12:34:21 +0530

    EvenOdd.cs

---
+++ EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class EvenOdd
+    {
+        public static void CalculateEvenOrOdd()
+        {
+            int i;
+            Console.WriteLine("Enter the Number");
+            i=int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is even Number");
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is an Odd Number");
+            }
+        }
+    }
+}
