b3d6e07bbc4ce39e5babe448b4b378734e68448f
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 12:35:27 +0530

    VowelOrConsonant.cs

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class VowelOrConsonant
+    {
+        public static void CalculateVowelorConsonant()
+        {
+            Console.WriteLine("Enter the Alphabet");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+        }
+    }
+}
